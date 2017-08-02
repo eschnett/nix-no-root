ecd5d69fefb5a39c1fa459034cfbe156397e936c
Erik Schnetter <schnetter@gmail.com>
2017-08-02 12:11:55 -0400

    Clean up script

--- scripts/nix-bootstrap-home.sh
+++ scripts/nix-bootstrap-home.sh
@@ -6,29 +6,29 @@
 mkdir -p $HOME/nix-boot
 cd $HOME/nix-boot
 
-if [ ! -e nix-1.11.13.tar.xz ] ; then
+if [ ! -e nix-1.11.13.tar.xz ]; then
 wget http://nixos.org/releases/nix/nix-1.11.13/nix-1.11.13.tar.xz
 fi
-if [ ! -e bzip2-1.0.6.tar.gz ] ; then
+if [ ! -e bzip2-1.0.6.tar.gz ]; then
 wget http://bzip.org/1.0.6/bzip2-1.0.6.tar.gz
 fi
-if [ ! -e curl-7.35.0.tar.lzma ] ; then
+if [ ! -e curl-7.35.0.tar.lzma ]; then
 wget http://curl.haxx.se/download/curl-7.35.0.tar.lzma
 fi
-if [ ! -e libseccomp-2.3.2.tar.gz ] ; then
+if [ ! -e libseccomp-2.3.2.tar.gz ]; then
 wget https://github.com/seccomp/libseccomp/releases/download/v2.3.2/libseccomp-2.3.2.tar.gz
 fi
-if [ ! -e sqlite-autoconf-3080300.tar.gz ] ; then
+if [ ! -e sqlite-autoconf-3080300.tar.gz ]; then
 wget http://www.sqlite.org/2014/sqlite-autoconf-3080300.tar.gz
 fi
-if [ ! -e DBI-1.631.tar.gz ] ; then
+if [ ! -e DBI-1.631.tar.gz ]; then
 # wget http://search.cpan.org/CPAN/authors/id/T/TI/TIMB/DBI-1.631.tar.gz
 wget --no-check-certificate https://pkgs.fedoraproject.org/repo/pkgs/perl-DBI/DBI-1.631.tar.gz/444d3c305e86597e11092b517794a840/DBI-1.631.tar.gz
 fi
-if [ ! -e DBD-SQLite-1.40.tar.gz ] ; then
+if [ ! -e DBD-SQLite-1.40.tar.gz ]; then
 wget --no-check-certificate https://pkgs.fedoraproject.org/repo/pkgs/perl-DBD-SQLite/DBD-SQLite-1.40.tar.gz/b9876882186499583428b14cf5c0e29c/DBD-SQLite-1.40.tar.gz
 fi
-if [ ! -e WWW-Curl-4.17.tar.gz ] ; then
+if [ ! -e WWW-Curl-4.17.tar.gz ]; then
 wget --no-check-certificate https://pkgs.fedoraproject.org/repo/pkgs/perl-WWW-Curl/WWW-Curl-4.17.tar.gz/997ac81cd6b03b30b36f7cd930474845/WWW-Curl-4.17.tar.gz
 fi
 
@@ -51,8 +51,8 @@ fi
 if [ ! -e $nix/bin/curl ]; then
    cd $nix
    lzma -d curl*.lzma
-   tar xvf curl*tar
-   rm curl*tar
+   tar xvf curl*.tar
+   rm curl*.tar
    cd curl-*
    ./configure --prefix=$nix
    make
@@ -77,7 +77,7 @@ if [ ! -e $nix/bin/sqlite3 ]; then
    make install
 fi
 
-if [ ! -e $nix/bin/dbiproxy ] ; then
+if [ ! -e $nix/bin/dbiproxy ]; then
   cd $nix
   tar xvzf DBI-*.tar.gz 
   cd DBI-*
@@ -104,7 +104,7 @@ if [ ! -e $nix/lib64/perl5/WWW/Curl.pm ]; then
   make install
 fi
 
-if [ ! -e $nix/bin/nix-env ] ; then
+if [ ! -e $nix/bin/nix-env ]; then
   cd $nix
   xz -d nix-*xz
   tar xvf nix-*.tar
